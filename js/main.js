824bd216ae8b1856a591e910a54a720d592c84df
Daisuke Murakami <daimura10@gmail.com>
2018-04-14 17:42:48 +0900

    updated profile

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  'use strict';
+  $('a[href =#]').click(function(){
+    var speed = 400;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+})();
