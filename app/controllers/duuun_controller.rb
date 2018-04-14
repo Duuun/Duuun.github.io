ede808f0833541caab1b87a64e527a2b36e43576
Daisuke Murakami <daimura10@gmail.com>
2018-04-14 10:39:11 +0900

    updated

---
+++ app/controllers/duuun_controller.rb
@@ -0,0 +1,2 @@
+class DuuunController < ApplicationController
+end
---
+++ test/controllers/duuun_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DuuunControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
